78408119c05abb314788f91de752b992478b95cb
Jeff Langr <jeff@langrsoft.com>
2023-10-24 14:17:07 -0600

    unpublic tests

--- src/test/java/scratch/ARectangle.java
+++ src/test/java/scratch/ARectangle.java
@@ -8,23 +8,23 @@ import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ARectangle {
+class ARectangle {
    private Rectangle rectangle;
 
    @AfterEach
-   public void ensureInvariant() {
+   void ensureInvariant() {
       assertTrue(constrainsSidesTo(rectangle, 100),
          format("rectangle invariant: both sides do not exceed 100"));
    }
 
-   private boolean constrainsSidesTo(Rectangle rect, int length) {
+   boolean constrainsSidesTo(Rectangle rect, int length) {
       return
          Math.abs(rect.origin().x() - rect.opposite().x()) <= length &&
          Math.abs(rect.origin().y() - rect.opposite().y()) <= length;
    }
 
    @Test
-   public void answersArea() {
+   void answersArea() {
       rectangle = new Rectangle(new Point(5, 5), new Point (15, 10));
       assertEquals(50, rectangle.area());
    }
@@ -34,7 +34,7 @@ public class ARectangle {
    @ExpectToFail
 // START:aRectangle
    @Test
-   public void allowsDynamicallyChangingSize() {
+   void allowsDynamicallyChangingSize() {
       rectangle = new Rectangle(new Point(5, 5));
       rectangle.setOppositeCorner(new Point(125, 55));
       assertEquals(6000, rectangle.area());
