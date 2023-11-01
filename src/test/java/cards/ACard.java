35e8d496156f8c3453549d47b1cfda035e9e5af2
Jeff Langr <jeff@langrsoft.com>
2023-11-01 09:54:22 -0600

    tests

--- src/test/java/cards/ACard.java
+++ src/test/java/cards/ACard.java
@@ -1,9 +1,12 @@
 package cards;
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ACard {
     @ParameterizedTest
@@ -18,5 +21,38 @@ class ACard {
     })
     void distillsToAString(int rank, String suit, String cardToStringExpected, String description) {
         assertEquals(cardToStringExpected, new Card(rank, suit).toString(), description);
+    }
+
+    @Nested
+    class CompareTo {
+        @Test
+        void returns0WhenSameCard() {
+            assertEquals(0, new Card(1, "S")
+                    .compareTo(new Card(1, "S")));
+        }
+
+        @Test
+        void returnsNegativeWhenRankLower() {
+            assertTrue(new Card(1, "S")
+                    .compareTo(new Card(3, "S")) < 0);
+        }
+
+        @Test
+        void returnsPositiveWhenRankHigher() {
+            assertTrue(new Card(11, "S")
+                    .compareTo(new Card(3, "S")) > 0);
+        }
+
+        @Test
+        void returnsNegativeWithSameRankEarlierAlphabeticalSuit() {
+            assertTrue(new Card(2, "C")
+                    .compareTo(new Card(3, "D")) < 0);
+        }
+
+        @Test
+        void returnsPositiveWithSameRankLaterAlphabeticalSuit() {
+            assertTrue(new Card(4, "S")
+                    .compareTo(new Card(4, "H")) > 0);
+        }
     }
 }
