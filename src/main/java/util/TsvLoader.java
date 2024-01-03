d938b809f3c19c5b6a97e25c8f33928f21d5e7af
Jeff L <jeff@langrsoft.com>
2024-01-03 11:23:03 -0700

    highlight

--- src/main/java/util/TsvLoader.java
+++ src/main/java/util/TsvLoader.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class TsvLoader {
+   // START_HIGHLIGHT
    public List<List<String>> loadFrom(BufferedReader reader) {
+   // END_HIGHLIGHT
       return reader.lines()
          .map(row -> row.split("\t"))
          .map(Arrays::asList)
