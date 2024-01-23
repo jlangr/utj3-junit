5f66983966cd1aca4e35ba7c655355d4c9dcb7fb
Jeff L <jeff@langrsoft.com>
2024-01-23 09:32:47 -0700

    does not throw

---
+++ src/test/java/scratch/ANameValidator.java
@@ -0,0 +1,48 @@
+package scratch;
+
+// START:test
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ANameValidator {
+   // END:test
+   // START:class
+   class NameValidationException extends RuntimeException {}
+
+   class NameValidator {
+      long commaCount(String s) {
+         return s.chars().filter(ch -> ch == ',').count();
+      }
+
+      void validate(String name) {
+         if (name.isEmpty() ||
+            commaCount(name) > 1)
+            throw new NameValidationException();
+      }
+   }
+   // END:class
+
+   // START:test
+   NameValidator validator = new NameValidator();
+
+   @Test
+   void throwsWhenNameIsEmpty() {
+      assertThrows(NameValidationException.class, () ->
+         validator.validate(""));
+   }
+
+   @Test
+   void throwsWhenNameContainsMultipleCommas() {
+      assertThrows(NameValidationException.class, () ->
+         validator.validate("Langr, Jeffrey,J."));
+   }
+
+   @Test
+   void doesNotThrowWhenNoErrorsExist() {
+      assertDoesNotThrow(() ->
+         validator.validate("Langr, Jeffrey J."));
+   }
+}
+// END:test
