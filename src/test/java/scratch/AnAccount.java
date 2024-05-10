e9aa114cc6e7bdeaf354a5d540e5e01a815d5c97
Jeff L <jeff@langrsoft.com>
2024-05-10 07:54:18 -0600

    aaa fixed

--- src/test/java/scratch/AnAccount.java
+++ src/test/java/scratch/AnAccount.java
@@ -37,21 +37,19 @@ class AnAccount {
    @Test
    void depositIncreasesBalance() {
       var initialBalance = account.getBalance();
+
       account.deposit(100);
 
-      var balance = account.getBalance();
-
-      assertTrue(balance > initialBalance);
+      assertTrue(account.getBalance() > initialBalance);
    }
 
    @Test
    void depositIncreasesBalanceByAmountOfDeposit() {
       account.deposit(50);
+
       account.deposit(100);
 
-      var balance = account.getBalance();
-
-      assertEquals(150, balance);
+      assertEquals(150, account.getBalance());
    }
 
    @Nested
