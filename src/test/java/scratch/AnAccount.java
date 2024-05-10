dc70a13403550efebb2347713b50c6cd8dd57312
Jeff L <jeff@langrsoft.com>
2024-05-10 07:36:39 -0600

    init

--- src/test/java/scratch/AnAccount.java
+++ src/test/java/scratch/AnAccount.java
@@ -13,52 +13,31 @@ class AnAccount {
    void createAccount() {
       account = new Account("an account name");
    }
+
+   @Test
+   void hasPositiveBalanceIsTrueAfterInitialDeposit() {
+      account.deposit(50);
+
+      assertTrue(account.hasPositiveBalance());
+   }
    // ...
    // END:before
 
-   // START:assertTrue0
-   @Test
-   void hasPositiveBalanceIsTrueAfterInitialDeposit() {
-      var account = new Account("an account name");
-
-      account.deposit(50);
-
-      Assertions.assertTrue(account.hasPositiveBalance());
-   }
-   // ...
-   // END:assertTrue0
-
-   // START:assertFalse
-   // START:assertTrueNot
    @Test
    void doesNotHavePositiveBalanceWhenAccountCreated() {
-      // END:assertFalse
       assertTrue(!account.hasPositiveBalance());
-      // END:assertTrueNot
-      // START:assertFalse
       assertFalse(account.hasPositiveBalance());
-      // START:assertTrueNot
    }
-   // END:assertFalse
-   // END:assertTrueNot
 
-   // START:assertTrue1
    @Test
    void hasPositiveBalanceAfterInitialDeposit() {
-      var account = new Account("an account name");
-
       account.deposit(50);
 
-      // START_HIGHLIGHT
       assertTrue(account.hasPositiveBalance());
-      // END_HIGHLIGHT
    }
-   // END:assertTrue1
 
-   // START:assertTrue2
    @Test
    void depositIncreasesBalance() {
-      var account = new Account("an account name");
       var initialBalance = account.getBalance();
       account.deposit(100);
 
@@ -66,9 +45,7 @@ class AnAccount {
 
       assertTrue(balance > initialBalance);
    }
-   // END:assertTrue2
 
-   // START:assertEquals0
    @Test
    void depositIncreasesBalanceByAmountOfDeposit() {
       account.deposit(50);
@@ -78,7 +55,6 @@ class AnAccount {
 
       assertEquals(150, balance);
    }
-   // END:assertEquals0
 
    @Nested
    class SillyAssertMessage {
@@ -89,7 +65,6 @@ class AnAccount {
 
       @ExpectToFail
       @Disabled
-      // START:worthlessAssertMessage
       @Test
       void balanceRepresentsTotalOfDeposits() {
          account.deposit(50);
@@ -99,7 +74,6 @@ class AnAccount {
 
          assertEquals(101, balance, "account balance must be total of deposits");
       }
-      // END:worthlessAssertMessage
    }
 
    @Test
@@ -119,33 +93,26 @@ class AnAccount {
 
    @Nested
    class TryCatchExceptions {
-      // START:tryException
       @Test
       void throwsWhenWithdrawingTooMuch() {
          try {
-            // START_HIGHLIGHT
             account.withdraw(100);
-            // END_HIGHLIGHT
             fail();
          } catch (InsufficientFundsException expected) {
             assertEquals("balance only 0", expected.getMessage());
          }
       }
-      // END:tryException
    }
 
    @Nested
    class NewSchoolExceptions {
-      // START:lambdaException
       @Test
       void throwsWhenWithdrawingTooMuch() {
          var thrown = assertThrows(InsufficientFundsException.class,
             () -> account.withdraw(100));
          assertEquals("balance only 0", thrown.getMessage());
       }
-      // END:lambdaException
    }
-
    // START:before
 }
 // END:before
